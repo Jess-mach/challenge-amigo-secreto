47730bddcecd7feff919d5ef3cd1f856c3b05887
Jess-mach <machadojessica1997@gmail.com>
2025-01-28 19:51:42 -0300

    Finalizado

--- app.js
+++ app.js
@@ -3,13 +3,49 @@
 let listaAmigos = [];
 
 
-function adicionarAmigo (){
-    let nomeAmigo = document.getElementById ('input').value;
-    if (nomeAmigo == ''){
-        alert ('Insira um nome');
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+
+    if (nomeAmigo == '') {
+        alert('Insira um nome');
         return;
     }
+
+    if (listaAmigos.includes(nomeAmigo)) {
+        alert('Este amigo já está na lista!');
+        return;
+    }
+
     listaAmigos.push(nomeAmigo);
+    document.getElementById('amigo').value = '';
+    atualizarLista();
 }
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+
+    for (let amigo of listaAmigos) {
+        lista.innerHTML += `<li>${amigo}</li>`;
+    }
+}
+function sortearAmigo() {
+    if (listaAmigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos no campo.')
+        return;
+    }
+
+    let sortearNome = Math.floor(Math.random() * listaAmigos.length);
+
+    let amigoSecreto = listaAmigos[sortearNome];
+
+    document.getElementById('resultado').innerHTML = 'Amigo Sorteado' + amigoSecreto;
+}
+
+
+
+
+
 
 
