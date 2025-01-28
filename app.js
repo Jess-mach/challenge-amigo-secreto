15abd3e54b33c26bf71c680e89c7acdd0500d857
Jess-mach <machadojessica1997@gmail.com>
2025-01-27 21:32:46 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+
+function adicionarAmigo (){
+    let nomeAmigo = document.getElementById ('input').value;
+    if (nomeAmigo == ''){
+        alert ('Insira um nome');
+        return;
+    }
+    listaAmigos.push(nomeAmigo);
+}
+
+
